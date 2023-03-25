9e865ff02fc93e07a681da6854955979c3ddf0f9
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-25 08:21:58 -0700

    ggg

--- printf.c
+++ printf.c
@@ -1,67 +1,65 @@
-#include <stdio.h>
 #include <stdarg.h>
-
+#include <stdio.h>
+#include "main.h"
 /**
- * _printf - custom implementation of printf function
- * @format: string with format specifiers
- *
- * Return: number of characters printed
+ * print_all - Prints anything.
+ * @format: A list of types of arguments passed to the function.
+ *          c: char
+ *          i: integer
+ *          f: float
+ *          s: char * (if the string is NULL, print (nil) instead)
+ * Return: void.
  */
 int _printf(const char *format, ...)
 {
-    va_list arg;
-    int printed_chars = 0;
-
-    va_start(arg, format);
-    while (*format)
-    {
-        if (*format == '%')
-        {
-            format++;
-            switch (*format)
-            {
-                case 'c':
-                {
-                    char c = (char) va_arg(arg, int);
-                    putchar(c);
-                    printed_chars++;
+	va_list args;
+	char c ;
+	char *s;
+	int i, printed;
+	if (format == NULL)
+	return (-1);
+	va_start(args, format);
+	while (format && format[i])
+	{
+	    if(format[i] == '%')
+	    {
+    		switch (format[i+1])
+    		   {
+    		       case '%':
+                    _putchar('%');
+    				i+=2;
+    				printed++;
                     break;
-                }
-                case 's':
-                {
-                    char *s = va_arg(arg, char *);
-                    if (s == NULL)
-                    {
-                        fputs("(null)", stdout);
-                        printed_chars += 6;
-                    }
-                    else
-                    {
-                        while (*s)
-                        {
-                            putchar(*s);
-                            s++;
-                            printed_chars++;
-                        }
-                    }
-                    break;
-                }
-                case '%':
-                {
-                    putchar('%');
-                    printed_chars++;
-                    break;
-                }
-            }
-        }
-        else
-        {
-            putchar(*format);
-            printed_chars++;
-        }
-        format++;
-    }
-    va_end(arg);
-
-    return printed_chars;
+    			case 'c':
+    			    c = (char) va_arg(args, int);
+    				_putchar(c);
+    				i+=2;
+    				printed++;
+    				break;
+    			case 's':
+    				s = va_arg(args, char *);
+    				if (s == NULL)
+    				{
+    				    i+=2;
+    					break;
+    				}
+    				printed+=_puts(s);
+    				 i+=2;
+    				break;
+    			default:
+    			    _putchar('%');
+    				i++;
+    				continue;
+    		   }
+    		}
+    		else
+    		{
+    		    c = format[i];
+    			_putchar(c);
+    		    printed++;
+    		    i++;
+    		}
+	    }
+	va_end(args);
+	return printed;
 }
