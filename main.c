27ca4082dcb5cff8ceeb82c50b8f22fccaaeb768
Houssamab54 <aboulfaraj1houssam@gmail.com>
2023-03-25 17:39:07 -0700

    ggg

---
+++ main.c
@@ -0,0 +1,29 @@
+#include <limits.h>
+#include <stdio.h>
+#include "main.h"
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0
+ */
+int main(void)
+{
+    int len;
+    unsigned int ui;
+    void *addr;
+
+    len = _printf("Let's try to printf a simple sentence.\n");
+    len2 = printf("Let's try to printf a simple sentence.\n");
+    _printf("Character:[%c]\n", 'H');
+    printf("Character:[%c]\n", 'H');
+    _printf("String:[%s]\n", "I am a string !");
+    printf("String:[%s]\n", "I am a string !");
+    _printf("");
+    printf("");
+    _printf("%%\n");
+    printf("%%\n");
+
+
+    return (0);
+}
